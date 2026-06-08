bfdb5442d901d217615600165661660e124b3b36
Luiz-Miguel01 <luiz.ceccon@alunos.ifsuldeminas.edu.br>
2026-06-08 10:33:19 -0300

    Delete aula 1 directory

--- aula 1/app1.py
+++
@@ -1,8 +0,0 @@
-#comando de entrada
-idade = input("Digite sua idade: ")
-
-#processamento + operador aritimético
-idade = idade + 1 
-
-#comando de saída
-print("No ano que vem você terá: ",idade, 'anos')
